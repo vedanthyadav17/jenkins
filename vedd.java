83042bf695b913e068582de4f04f62415bc59c0c
vedanthyadav17 <vedanthyadav17@gmail.com>
2026-04-08 14:42:28 +0530

    Add HelloWorld class with user input functionality

---
+++ vedd.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Print a message
+        System.out.println("Hello, World!");
+
+        // Take user input
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        // Print personalized message
+        System.out.println("Nice to meet you, " + name + "!");
+        
+        scanner.close();
+    }
+}
